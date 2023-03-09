b2e0d6d756150ddf8a2cdaba137fe8f7e31ea30a
Eric Wang <flpha0830@gmail.com>
2023-03-09 17:47:47 -0600

    [mlir] remove unused part

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -50,7 +50,6 @@ llvm_config.with_environment('PATH', config.llvm_tools_dir, append_path=True)
 
 tool_dirs = [config.toy_tools_dir, config.llvm_tools_dir]
 tools = [
-    'toy-capi-test',
     'toy-opt',
     'toy-translate',
 ]
